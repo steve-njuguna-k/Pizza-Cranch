8fb2fb95d4960fa753f080484b003a10c41e1335
Steve Njuguna <stevenjuguna93@gmail.com>
2021-06-27 20:14:01 +0300

    Added variables & arrays for pizza prices, crust price & delivery price

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,12 @@ $(document).ready(function(){
         $("#" + $(this).val()).show();
     })
 })
+
+let chickenBeefPepperoniPrices = [300, 550, 750, 950];
+let chickenHawaiianPrices = [350, 600, 800, 1000];
+let chickenMaconBBQPrices = [400, 650, 850, 1050];
+let meatDeluxePrices = [450, 700, 900, 1100];
+let crispyPrice = 200;
+let stuffedPrice = 350;
+let glutenFreePrice = 500;
+let deliveryTransport = 500;
